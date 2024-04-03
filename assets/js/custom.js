cbf75ddb2748b26b67fd29673a5561662fbacfb3
Sudheer <121391508+sudheerreddysabbella@users.noreply.github.com>
2024-04-03 22:30:27 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,18 @@
-var bg = document.getElementById("pick-bg"),
-  images = [
-    "assets/bg-images/3.svg",
-    "assets/bg-images/8.svg",
-    "assets/bg-images/11.svg",
-    "assets/bg-images/16.svg",
-    "assets/bg-images/20.svg",
-    "assets/bg-images/23.svg",
-  ],
-  countImages = images.length,
+var bg = document.getElementById("pick-bg");
+var images = [
+  "assets/bg-images/3.svg",
+  "assets/bg-images/8.svg",
+  "assets/bg-images/11.svg",
+  "assets/bg-images/16.svg",
+  "assets/bg-images/20.svg",
+  "assets/bg-images/23.svg",
+];
+var countImages = images.length,
   index = Math.floor(Math.random() * countImages);
 
 var btn = document.getElementById("button"),
   pick = document.getElementById("pick-para"),
+  con = document.getElementsByClassName("content"),
   em = document.getElementById("emo");
 
 btn.addEventListener("click", async () => {
@@ -19,8 +20,15 @@ btn.addEventListener("click", async () => {
     em.innerText = "😸";
     var data = await fetch("https://rizzapi.vercel.app/random");
     var Json = await data.json();
+    pick.classList.add("hide");
+    // con[0].style.width = "auto";
     // DOM Manipulation
-    pick.innerText = Json.text;
+    setTimeout(() => {
+      pick.innerText = Json.text;
+      pick.classList.remove("hide");
+      con[0].style.width = "auto";
+      con[0].style.height = "auto";
+    }, 500);
     pick.style.marginBottom = 26 + "px";
     bg.style.animationName = "Blur-beat";
     em.innerText = "😻";
@@ -31,3 +39,4 @@ btn.addEventListener("click", async () => {
 window.onload = () => {
   bg.style.backgroundImage = "url(" + images[index] + ")";
 };
+ 
